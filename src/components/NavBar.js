ea1c0e22b717d6ad020119ec1e0a2fe62d53632b
KelvinNaikuni <kelvin.naikuni@student.moringaschool.com>
2023-10-27 07:32:00 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (<div>
+    {links.map((link, index) => (
+    <a key={index} href={`#${link.toLowerCase()}`}>{link.toLowerCase()}</a>
+  ))}</div>
+  );
 }
 
 export default NavBar;
